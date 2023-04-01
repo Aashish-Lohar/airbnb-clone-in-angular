ff6bdbabbbd8abfbe5e5d1635ff2922439d49947
aashish_lohar <aashishlohar111@gmail.com>
2023-04-01 20:39:49 +0530

    remove ignite ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,28 +9,20 @@ import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { StaysComponent } from './stays/stays.component';
 import { StayPageComponent } from './stays/stay-page/stay-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CalendarComponent } from './calendar/calendar.component';
-import { IgxCalendarModule, IgxSnackbarModule, IgxDialogModule,IgxButtonModule, IgxProgressBarModule } from 'igniteui-angular';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FiltersComponent,
     StaysComponent,
-    StayPageComponent,
-    CalendarComponent
+    StayPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SlickCarouselModule,
     BrowserAnimationsModule,
-    HammerModule,
-    IgxCalendarModule,
-	  IgxSnackbarModule,
-    IgxDialogModule,
-    IgxButtonModule,
-    IgxProgressBarModule,
+    HammerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
